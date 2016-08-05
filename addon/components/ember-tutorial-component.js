e25d6a0fdc0644b1bd6fc74fdf8626d21b422d3f
Shrinivas Kangal <skangal@yahoo-inc.com>
2016-08-05 15:49:55 -0700

    Next iteration.

--- addon/components/ember-tutorial-component.js
+++ addon/components/ember-tutorial-component.js
@@ -107,18 +107,6 @@ export default Ember.Component.extend({
 
 
   didReceiveAttrs() {
-    this.set('config', {
-      data: [{
-        eleId: 'test1',
-        message: 'This is a demo message. May be step 1, please press next.'
-      },{
-        eleId: 'test2',
-        message: 'Message 2'
-      },{
-        eleId: 'test3',
-        message: 'This press next mo message. May be step 1, pleas'
-      }]
-    });
   },
 
   didInsertElement() {
