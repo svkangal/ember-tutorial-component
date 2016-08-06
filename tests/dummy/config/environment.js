396cede6ef83b4748384ee33fcb5d99b646e5d21
Shrinivas Kangal <skangal@yahoo-inc.com>
2016-08-05 22:43:04 -0700

    Updated cool url for test

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -32,7 +32,7 @@ module.exports = function(environment) {
 
    if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/ember-flatpickr/';
+    ENV.rootURL = '/ember-tutorial-component/';
   }
 
 
