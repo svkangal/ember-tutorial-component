627893bddf8fc397146ac27d29471466ada9f687
Shrinivas Kangal <skangal@yahoo-inc.com>
2016-08-05 23:31:08 -0700

    Pages test

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,13 +4,13 @@ export default Ember.Controller.extend({
   config: {
     data: [{
       eleId: 'test1',
-      message: 'First help message goes here.'
+      message: 'First help message goes here. Show some helpful message here.'
     }, {
       eleId: 'test2',
-      message: 'This is the next step.'
+      message: 'This is the next step. Show some helpful message here.'
     }, {
       eleId: 'test3',
-      message: 'Final step, hit done to hide.'
+      message: 'Final step, hit done to hide. Show some helpful message here.'
     }]
   }
 });
