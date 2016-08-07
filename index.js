502b0f669a54d4006a83568eb2d6e25432a60075
Shrinivas Kangal <skangal@yahoo-inc.com>
2016-08-06 17:54:08 -0700

    Basic acceptance test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'ember-tutorial-component',
 
   contentFor: function(type, config) {
-    if (config.environment !== 'test' && type === 'body-footer') {
+    if (type === 'body-footer') {
       return '<div id="ember-tutorial-component-wormhole"></div>';
     }
   },
---
+++ tests/acceptance/basic-acceptance-test-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import Ember from 'ember';
+import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | basic acceptance test');
+
+test('visiting /', function(assert) {
+  assert.expect(1);
+  visit('/');
+  andThen(function() {
+    assert.equal(Ember.$('.tutorial-component > div')[0].innerHTML, 'First help message goes here. Show some helpful message here.');
+  });
+});
