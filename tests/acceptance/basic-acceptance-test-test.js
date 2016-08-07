dabed1fe0959b8179a72c100ccd6b0e78d4154c8
Shrinivas Kangal <skangal@yahoo-inc.com>
2016-08-06 18:49:00 -0700

    Basic acceptance test

--- tests/acceptance/basic-acceptance-test-test.js
+++ tests/acceptance/basic-acceptance-test-test.js
@@ -5,9 +5,10 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | basic acceptance test');
 
 test('visiting /', function(assert) {
-  assert.expect(1);
+  assert.expect(2);
   visit('/');
   andThen(function() {
     assert.equal(Ember.$('.tutorial-component > div')[0].innerHTML, 'First help message goes here. Show some helpful message here.');
+    assert.equal(Ember.$('.tutorial-component > button')[0].innerHTML, 'Next');
   });
 });
