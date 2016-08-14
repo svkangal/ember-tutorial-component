78e4c87f08970aaab90ab9d2783edb72896b7271
Shrinivas Kangal <svkangal@users.noreply.github.com>
2016-08-13 18:07:49 -0700

    Fixing tests

--- tests/acceptance/basic-acceptance-test-test.js
+++ tests/acceptance/basic-acceptance-test-test.js
@@ -5,10 +5,11 @@ import moduleForAcceptance from '../../tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | basic acceptance test');
 
 test('visiting /', function(assert) {
-  assert.expect(2);
+  assert.expect(3);
   visit('/');
   andThen(function() {
     assert.equal(Ember.$('.tutorial-component > div')[0].innerHTML, 'First help message goes here. Show some helpful message here.');
-    assert.equal(Ember.$('.tutorial-component > button')[0].innerHTML, 'Next');
+    assert.equal(Ember.$('.tutorial-component > button')[0].innerHTML, 'Skip');
+    assert.equal(Ember.$('.tutorial-component > button')[1].innerHTML, 'Next');
   });
 });
